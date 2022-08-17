3d6037dac0a273806a926e5ae70f8771fd9d574b
Ruel_Lobo <ruelwolf@gmail.com>
2022-08-17 19:56:15 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// // for(let i = 1; i<5; i++){
+// //   console.log(i);
+// // }
+
+// let x = 1;
+// // while(x <= 5){
+// //   console.log(x);
+// //   x++;
+// // }
+
+
+let x = 1;
+do{
+  console.log(x);
+  x++;
+}while(x <= 5);
+
+
+
+let asterisk = "*";
+let n = 1;
+
+// using for loop
+// // for(let i = 1; i <= 5; i++){
+// //   console.log(asterisk);
+// //   asterisk += '*';
+// // }
+
+
+//  using while loop
+// while (n <= 5) {
+//   console.log(asterisk);
+//   asterisk += '*';
+//   n++;
+// }
+
+
+// using do while loop
+do {
+  console.log(asterisk);
+  asterisk += "*";
+  n++;
+} while (n <= 6);
+
+
